cc380ee5e41d8b981002a7614ef0283d3798c67d
12darko <fallenwar12@gmail.com>
2019-02-25 14:30:14 +0300

    Add files via upload

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Hesap Teklifi</title>
+	<title> Video Script</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/front/style.css">
 </head>
