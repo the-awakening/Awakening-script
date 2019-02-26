1f28ea5de9ba660c551df0ea7c348d788de4368f
12darko <fallenwar12@gmail.com>
2019-02-26 19:09:52 +0300

    Add files via upload

--- menu.php
+++ menu.php
@@ -21,31 +21,10 @@
 
          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
           <li class="dropdown order-1">
-            <a href="#"  id=" dropdownMenu1" data-toggle="dropdown"   class=" nav-link tab2 ml-auto text-white">
+            <a href="#"  data-toggle="modal" data-target="#modalLRForm"  class=" nav-link tab2 ml-auto text-white">
               <i class="far fa-user-circle fa-lg text-white"></i>
 
-              <ul class="bg-login-form dropdown-menu dropdown-menu-right mt-2 px-3 py-4"">
-
-                <form style="position: relative; bottom: 25px; height: 150px; "  >
-                  <div >
-                    <input name="k_ad" type="text"  placeholder="Kullanıcı Adı" style="position: relative; left: 0px; top: 25px; height: 32px !important;" />
-                  </div>
-                  <div >
-                    <input name="k_sifre" type="password" placeholder="Şifre" style="position: relative; left: 0px; top: 40px;  height: 32px !important;  "/>
-                  </div>
-                  <div >
-                    <input onclick="giris();" style="position: relative; top: 55px;"  type="submit" value="Giriş yap" />
-
-                  </div>
-
-                  <small><a href="#modalunuttum"   class="form-group text-left login_form_text_color"   style="position: relative; top: 65px;">Şifreni mi unuttun ?</a></small>
-                  &emsp;  
-
-                  <small><a href="#modalregister"   class="form-group text-right login_form_text_color"   style="position: relative; top: 65px;">Kayıt Ol</a></small>
-
-                </form>
-
-              </ul>
+             </a>
             </li>
 
 
